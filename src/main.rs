099e7a89e7723f7fdf0983c726880896b6b5466f
Denis Evsyukov <denis@evsyukov.org>
2020-07-29 11:14:13 +0300

    [~] clap -> structopt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,52 @@
-use clap::{App, Arg};
 use colored::Colorize;
 use rand::Rng;
 use rust_embed::RustEmbed;
 use std::collections::HashMap;
+use structopt::StructOpt;
 
 #[derive(RustEmbed)]
 #[folder = "diceware/"]
 struct Asset;
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "passgen",
+    version = "1.0.0",
+    about = "Password genrator (random vs diceware)"
+)]
+struct Opt {
+    #[structopt(short, long, default_value = "24", help = "Passwords length")]
+    length: isize,
+
+    #[structopt(short, long, default_value = "7", help = "Number of passwords")]
+    number: isize,
+
+    #[structopt(short, long, help = "Using digits in passwords")]
+    digit: bool,
+
+    #[structopt(short, long, help = "Using symbols in passwords")]
+    symbols: bool,
+
+    #[structopt(subcommand, help = "Using diceware for passwords")]
+    diceware: Option<DicewareEnum>,
+}
+
+#[derive(StructOpt)]
+enum DicewareEnum {
+    Diceware {
+        #[structopt(
+            short,
+            long,
+            help = "Using eff dictionary for passwords, by default used official diceware dictionary"
+        )]
+        eff: bool,
+
+        #[structopt(short, long, default_value = "7", help = "Count of words in passwords")]
+        words: isize,
+        #[structopt(short, long, default_value = "7", help = "Number of passwords")]
+        number: isize,
+    },
+}
 
 fn check_pass_len(len: isize) -> isize {
     if len < 1 {
@@ -94,88 +134,18 @@ fn generate_diceware_passwords(word_count: isize, number: isize, eff: bool) {
 }
 
 fn main() {
-    let matches = App::new("passgen")
-        .version("1.0.0")
-        .about("Password genrator (random vs diceware)")
-        .arg(
-            Arg::with_name("LENGTH")
-                .short('l')
-                .long("length")
-                .about("Passwords length")
-                .default_value("24"),
-        )
-        .arg(
-            Arg::with_name("NUMBER")
-                .short('n')
-                .long("number")
-                .about("Number of passwords")
-                .default_value("7"),
-        )
-        .arg(
-            Arg::with_name("digit")
-                .short('d')
-                .long("digit")
-                .about("Using digits in passwords"),
-        )
-        .arg(
-            Arg::with_name("symbols")
-                .short('s')
-                .long("symbols")
-                .about("Using symbols in passwords"),
-        )
-        .subcommand(
-            App::new("diceware")
-                .about("Using diceware for passwords")
-                .arg(Arg::with_name("eff").short('e').long("eff").about(
-                    "Using eff dictionary for passwords, by default used official diceware dictionary",
-                ))
-                .arg(
-                    Arg::with_name("words")
-                        .short('w')
-                        .long("words")
-                        .default_value("7")
-                        .about("Count of words in passwords"),
-                )
-                .arg(
-                    Arg::with_name("NUMBER")
-                        .short('n')
-                        .long("number")
-                        .about("Number of passwords")
-                        .default_value("7"),
-                ),
-        )
-        .get_matches();
-
-    match matches.subcommand_name() {
-        Some("diceware") => {
-            let words_count = matches
-                .subcommand_matches("diceware")
-                .unwrap()
-                .value_of("words")
-                .unwrap()
-                .parse()
-                .unwrap();
-            let number = matches
-                .subcommand_matches("diceware")
-                .unwrap()
-                .value_of("NUMBER")
-                .unwrap()
-                .parse()
-                .unwrap();
-            let eff = matches
-                .subcommand_matches("diceware")
-                .unwrap()
-                .is_present("eff");
-            generate_diceware_passwords(words_count, number, eff);
+    let opt = Opt::from_args();
+    if let Some(diceware) = opt.diceware {
+        match diceware {
+            DicewareEnum::Diceware { eff, words, number } => {
+                generate_diceware_passwords(words, number, eff)
+            }
         }
-        None => {
-            let password_len = check_pass_len(matches.value_of("LENGTH").unwrap().parse().unwrap());
-            let password_count =
-                check_pass_number(matches.value_of("NUMBER").unwrap().parse().unwrap());
-            let digit: bool = matches.is_present("digit");
-            let symbols: bool = matches.is_present("symbols");
-            generate_random_passwords(password_count, password_len, digit, symbols);
-        }
-        _ => println!("Whats are fucking your doing?"),
+    } else {
+        let password_len = check_pass_len(opt.length);
+        let password_count = check_pass_number(opt.number);
+        let digit: bool = opt.digit;
+        let symbols: bool = opt.symbols;
+        generate_random_passwords(password_count, password_len, digit, symbols);
     }
 }
